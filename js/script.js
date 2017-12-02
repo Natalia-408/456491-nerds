26b4f59c3608ec47cac4234a826c814054d1eee6
Наталия Алексеева <nn2005n@rambler.ru>
2017-12-02 19:27:36 +0300

    Попап карты и пагинатор

---
+++ js/script.js
@@ -0,0 +1,36 @@
+var link = document.querySelector(".feedback");
+    var popup = document.querySelector(".container-form");
+	var close = popup.querySelector(".btn-form-close");
+	var form = popup.querySelector("form");
+	var login = popup.querySelector("[name=login]");
+	var email = popup.querySelector("[name=email]");
+	var storage = localStorage.getItem("login");
+	
+    link.addEventListener("click", function (evt) {
+		evt.preventDefault();
+		popup.classList.add("container-form-show");
+		 
+      login.focus();
+  	});
+	
+	close.addEventListener("click", function (evt) {
+		evt.preventDefault();
+		popup.classList.remove("container-form-show");
+	});
+	
+	form.addEventListener("submit", function (evt) {
+    if (!login.value || !email.value) {
+      evt.preventDefault();
+      console.log("Нужно ввести логин и пароль");
+    } else {
+      localStorage.setItem("login", login.value);
+    }
+  });
+  
+  window.addEventListener("keydown", function (evt) {
+    if (evt.keyCode === 27) {
+      if (popup.classList.contains("container-form-show")) {
+        popup.classList.remove("container-form-show");
+      }
+    }
+  });
